f4784cb1d193241bef0c238f1b98b0929fa99f9c
Wirachai <wirachai.me@gmail.com>
2017-07-23 22:29:02 +0700

    Add WorkshopMock

---
+++ WorkshopMock/Repositories/AccountRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkshopMock.Repositories
+{
+    public class AccountRepository
+    {
+        public decimal GetBalance(string accountId)
+        {
+            // fake result
+            return 100;
+        }
+
+        public void Edit(string accountId, decimal money)
+        {
+        }
+    }
+}
---
+++ WorkshopMock/Services/BankAccountService.cs
@@ -0,0 +1,28 @@
+﻿using WorkshopMock.Repositories;
+
+namespace WorkshopMock.Services
+{
+    public class BankAccountService
+    {
+        public AccountRepository AccountRepository { get; set; }
+
+        public BankAccountService()
+        {
+            AccountRepository = new AccountRepository();
+        }
+
+        public BankAccountService(AccountRepository repo)
+        {
+            AccountRepository = repo;
+        }
+
+        public void Withdraw(string accountId, decimal money)
+        {
+            var balance = AccountRepository.GetBalance(accountId);
+            if (balance >= money)
+            {
+                AccountRepository.Edit(accountId, balance - money);
+            }
+        }
+    }
+}
