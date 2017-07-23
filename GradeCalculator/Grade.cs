f20defe1d533178ff9ea684e6377bafcd2f63cf7
Wirachai <wirachai.me@gmail.com>
2017-07-23 22:01:23 +0700

    Add GradeCalculator workshop

---
+++ GradeCalculator/Grade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeCalculator
+{
+    public class Grade
+    {
+        public string GetGrade(int score)
+        {
+            return "F";
+        }
+    }
+}
