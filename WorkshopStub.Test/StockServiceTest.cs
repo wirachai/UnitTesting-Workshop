c4e5b24288b74b39d09f77290b10c5ad3ebffd46
Wirachai <wirachai.me@gmail.com>
2017-07-23 23:06:31 +0700

    Use moq for stub workshop

--- WorkshopStub.Test/StockServiceTest.cs
+++ WorkshopStub.Test/StockServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using WorkshopStub.Repositories;
 using WorkshopStub.Services;
 
@@ -11,10 +12,7 @@ namespace WorkshopStub.Test
         public void CheckAvailable_ShouldReturnTrue_WhenQuantityGreaterThan0()
         {
             // arrange
-            var stub = new ItemRepository_Stub();
-            stub.ReturnObject = new Models.ItemModel() { Quantity = 1 };
-
-            StockService service = new StockService(stub);
+            StockService service = SetUpServiceWithStub(1);
 
             // act
             var result = service.CheckAvailable("Some Id");
@@ -27,16 +25,23 @@ namespace WorkshopStub.Test
         public void CheckAvailable_ShouldReturnFalse_WhenQuantityIsZero()
         {
             // arrange
-            var stub = new ItemRepository_Stub();
-            stub.ReturnObject = new Models.ItemModel() { Quantity = 0 };
-
-            StockService service = new StockService(stub);
+            StockService service = SetUpServiceWithStub(0);
 
             // act
             var result = service.CheckAvailable("Some Id");
 
             // assert
             Assert.AreEqual(false, result);
+        }
+
+        private StockService SetUpServiceWithStub(int quantity)
+        {
+            var stub = new Mock<IItemRepository>();
+            stub.Setup(x => x.Get("Some Id"))
+                .Returns(new Models.ItemModel() { Quantity = quantity });
+
+            StockService service = new StockService(stub.Object);
+            return service;
         }
     }
 }
